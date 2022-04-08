caec30a45c0bfb60a711f681bb7eb4d47e83812b
stephen <kernan.software@gmail.com>
2022-04-07 20:08:23 -0500

    add media

--- components/PhotoGrid/PhotoGrid.js
+++ components/PhotoGrid/PhotoGrid.js
@@ -1,15 +1,21 @@
 import home from "../../styles/Home.module.scss";
 import photoGrid from "./PhotoGrid.module.scss";
-export const PhotoGrid = ({ variant, photos, title }) => {
+export const PhotoGrid = ({ variant, photos, title, useRawImage=false }) => {
   return (
     <div className={`${home.pane} ${home[variant]}`}>
-      <div className={photoGrid.title} >
+      <div className={photoGrid.title}>
         <h2>{title}</h2>
       </div>
       <div className={photoGrid.gridContainer}>
         {photos.map((photo) => (
-          <div className={photoGrid.gridImage} key={photo.url}></div>
-          // <img src={photo.url} alt={photo.alt} key={photo.url} className={photoGrid.gridImage} />
+          // <div className={photoGrid.gridImage} key={photo.url}></div>
+          <div className={useRawImage ?  null : photoGrid.photoContainer}  key={photo.url}>
+            <img
+              src={photo.url}
+              alt={photo.alt}
+              className={photoGrid.gridImage}
+            />
+          </div>
         ))}
       </div>
     </div>
